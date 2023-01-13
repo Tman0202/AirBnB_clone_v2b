540f0b9737d942ff717d690d57daf78c7cffd9f0
Tman0202 <tomasmandefro@gmail.com>
2023-01-13 07:17:20 -0800

    shh

--- models/base_model.py
+++ models/base_model.py
@@ -12,13 +12,13 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models
     Attributes:
-        id (sqlalchemy String): The BaseModel id.
+        id (sqlalchemy String): The BaseModel id.S
         created_at (sqlalchemy DateTime): The datetime at creation.
         updated_at (sqlalchemy DateTime): The datetime of last update.
     """
     id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    created_at = Column(DateTime, nullable=False, default=DATETIME().utcnow())
+    updated_at = Column(DateTime, nullable=False, default=DATETIME().utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
